e490a97ae1e6bc52c6f1a7e4fd1b7f484190798f
Aono05 <k.aono0522@gmail.com>
2023-05-18 16:11:57 +0900

    [add] Add validation to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
+
+  validates :email, presence: true
+  validates :password, presence: true
+  validate :password_complexity
+
+  def password_complexity
+    return if password.blank? || password =~ /^(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,64}$/
+
+    errors.add :password, 'Complexity requirement not met. Length should be 6-64 characters and include: 1 lowercase, 1 digit and 1 special character'
+  end
 end
