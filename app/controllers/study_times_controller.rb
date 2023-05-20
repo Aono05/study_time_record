36c97c69f2369ec75cd2cede39ac945678cef022
Aono05 <k.aono0522@gmail.com>
2023-05-20 14:49:17 +0900

    [add] Add new and create actions

--- app/controllers/study_times_controller.rb
+++ app/controllers/study_times_controller.rb
@@ -4,4 +4,26 @@ class StudyTimesController < ApplicationController
   def index
     @study_times = current_user.study_times
   end
+
+  def new
+    @study_time = StudyTime.new
+  end
+
+  def create
+    @study_time = StudyTime.new(study_time_params)
+    @study_time.user = current_user
+
+    if @study_time.save
+      redirect_to @study_time
+    else
+      render 'new'
+    end
+  end
+
+
+  private
+
+  def study_time_params
+    params.require(:study_time).permit(:start_time, :end_time)
+  end
 end
