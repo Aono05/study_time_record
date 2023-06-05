9a5b6c51c2fc735185a9f8171d2a473c2d98a47d
Aono05 <k.aono0522@gmail.com>
2023-06-05 13:34:26 +0900

    [add] Add logic for bar charting

--- app/models/study_time.rb
+++ app/models/study_time.rb
@@ -4,4 +4,17 @@ class StudyTime < ApplicationRecord
   def duration
     (ended_at - started_at) / 60
   end
+
+
+  private
+
+  class << self
+    def total_duration_per_day(user)
+      StudyTime.group("date(started_at)").where(user: user).sum(calculate_duration)
+    end
+
+    def calculate_duration
+      "(strftime('%s', ended_at) - strftime('%s', started_at)) / 60"
+    end
+  end
 end
