de4a60cea0386670ec51ab8bd761874ce5843ae7
Aono05 <k.aono0522@gmail.com>
2023-06-21 06:34:52 +0900

    [update] early return

--- app/models/study_time.rb
+++ app/models/study_time.rb
@@ -2,18 +2,20 @@ class StudyTime < ApplicationRecord
   belongs_to :user
 
   def duration
+    return 0 if ended_at.nil?
+    return 0 if ended_at < started_at
+
     (ended_at - started_at) / 60
   end
 
-
-  private
-
   class << self
     def total_duration_per_day(user)
-      StudyTime.group("date(started_at)").where(user: user).sum(calculate_duration)
+      group("date(started_at)").where(user: user).sum(calcurate_duration)
     end
 
-    def calculate_duration
+    private
+
+    def calcurate_duration
       "(strftime('%s', ended_at) - strftime('%s', started_at)) / 60"
     end
   end
