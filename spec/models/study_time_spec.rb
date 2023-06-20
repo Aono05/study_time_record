d78f55ec8da209efc7e3a5f9dd0634083351c83b
Aono05 <k.aono0522@gmail.com>
2023-06-21 06:37:25 +0900

    [update] Add test contents

--- spec/models/study_time_spec.rb
+++ spec/models/study_time_spec.rb
@@ -2,12 +2,42 @@ require 'rails_helper'
 
 RSpec.describe StudyTime, type: :model do
   describe "#duration" do
-    let(:started_at) { Time.zone.local(2023, 5, 25, 6, 0, 0) }
-    let(:ended_at) { Time.zone.local(2023, 5, 25, 6, 1, 0) }
-    let(:study_time) { StudyTime.new(started_at: started_at, ended_at: ended_at) }
+    context "終了時間が設定されている場合" do
+      context "開始時間より終了時間が遅い場合" do
+        let(:study_time) { StudyTime.new(started_at: Time.zone.local(2023, 5, 25, 6, 0, 0), ended_at: Time.zone.local(2023, 5, 25, 6, 1, 0)) }
+        let(:expected) { 1 }
 
-    it "calculates the duration correctly in minutes" do
-      expect(study_time.duration).to eq(1)
+        it "1分と表示される" do
+          expect(study_time.duration).to eq(expected)
+        end
+      end
+
+      context "開始時間と終了時間が同じ場合" do
+        let(:study_time) { StudyTime.new(started_at: Time.zone.local(2023, 5, 25, 6, 0, 0), ended_at: Time.zone.local(2023, 5, 25, 6, 0, 0)) }
+        let(:expected) { 0 }
+
+        it "0分と表示される" do
+          expect(study_time.duration).to eq(expected)
+        end
+      end
+
+      context "開始時間が終了時間より後の場合" do
+        let(:study_time) { StudyTime.new(started_at: Time.zone.local(2023, 5, 25, 6, 2, 0), ended_at: Time.zone.local(2023, 5, 25, 6, 1, 0)) }
+        let(:expected) { 0 }
+
+        it "0分と表示される" do
+          expect(study_time.duration).to eq(expected)
+        end
+      end
+    end
+
+    context "終了時間が設定されていない場合" do
+      let(:study_time) { StudyTime.new(started_at: Time.zone.local(2023, 5, 25, 6, 2, 0), ended_at: nil) }
+      let(:expected) { 0 }
+
+      it "0分と表示される" do
+        expect(study_time.duration).to eq(expected)
+      end
     end
   end
 end
