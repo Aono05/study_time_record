cfaa8c0d0b611e7b301c20c914a4d9cc927b2a82
Aono05 <k.aono0522@gmail.com>
2023-06-24 14:15:35 +0900

    [update] The same process is commonized.

--- spec/models/study_time_spec.rb
+++ spec/models/study_time_spec.rb
@@ -3,8 +3,11 @@ require 'rails_helper'
 RSpec.describe StudyTime, type: :model do
   describe "#duration" do
     context "終了時間が設定されている場合" do
+        let(:study_time) { StudyTime.new(started_at: started_at, ended_at: ended_at) }
+
       context "開始時間より終了時間が遅い場合" do
-        let(:study_time) { StudyTime.new(started_at: Time.zone.local(2023, 5, 25, 6, 0, 0), ended_at: Time.zone.local(2023, 5, 25, 6, 1, 0)) }
+        let(:started_at) { Time.zone.local(2023, 5, 25, 6, 0, 0) }
+        let(:ended_at) { Time.zone.local(2023, 5, 25, 6, 1, 0) }
         let(:expected) { 1 }
 
         it "1分と表示される" do
@@ -13,7 +16,8 @@ RSpec.describe StudyTime, type: :model do
       end
 
       context "開始時間と終了時間が同じ場合" do
-        let(:study_time) { StudyTime.new(started_at: Time.zone.local(2023, 5, 25, 6, 0, 0), ended_at: Time.zone.local(2023, 5, 25, 6, 0, 0)) }
+        let(:started_at) { Time.zone.local(2023, 5, 25, 6, 0, 0) }
+        let(:ended_at) { Time.zone.local(2023, 5, 25, 6, 0, 0) }
         let(:expected) { 0 }
 
         it "0分と表示される" do
@@ -22,7 +26,8 @@ RSpec.describe StudyTime, type: :model do
       end
 
       context "開始時間が終了時間より後の場合" do
-        let(:study_time) { StudyTime.new(started_at: Time.zone.local(2023, 5, 25, 6, 2, 0), ended_at: Time.zone.local(2023, 5, 25, 6, 1, 0)) }
+        let(:started_at) { Time.zone.local(2023, 5, 25, 6, 1, 0) }
+        let(:ended_at) { Time.zone.local(2023, 5, 25, 6, 0, 0) }
         let(:expected) { 0 }
 
         it "0分と表示される" do
@@ -32,7 +37,9 @@ RSpec.describe StudyTime, type: :model do
     end
 
     context "終了時間が設定されていない場合" do
-      let(:study_time) { StudyTime.new(started_at: Time.zone.local(2023, 5, 25, 6, 2, 0), ended_at: nil) }
+      let(:study_time) { StudyTime.new(started_at: started_at, ended_at: ended_at) }
+      let(:started_at) { Time.zone.local(2023, 5, 25, 6, 0, 0) }
+      let(:ended_at) { nil }
       let(:expected) { 0 }
 
       it "0分と表示される" do
