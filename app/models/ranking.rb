841a6dd56d18f1c99dc9899a75a4e6ef5286317c
Aono05 <k.aono0522@gmail.com>
2023-08-19 15:12:10 +0900

    [fix] Fixed ranking creation method. Adopted bulk insert and find_each for use with large amounts of data.

--- app/models/ranking.rb
+++ app/models/ranking.rb
@@ -6,14 +6,30 @@ class Ranking < ApplicationRecord
       now = Time.current
       chunk_id = now.to_i
 
-      rankings = StudyTime
-        .total_duration_per_user(now.ago(1.week), now)
-        .map do |user_id, total_duration|
-          { user_id: user_id, total_duration: total_duration, chunk_id: chunk_id }
+      results = {}
+      StudyTime.where_by_duration(
+        now.ago(1.week),
+        now
+      ).find_each do |study_time|
+        aggregated_total_duration = results[study_time.user_id]
+
+        results[study_time.user_id] = if aggregated_total_duration.nil?
+          study_time.duration
+        else
+          aggregated_total_duration + study_time.duration
         end
+      end
+
+      rankings = results.map do |user_id, total_duration|
+        {
+          user_id: user_id,
+          chunk_id: chunk_id,
+          total_duration: total_duration.to_i
+        }
+      end
 
       Ranking.import(rankings)
-      end
+    end
 
     def total_week_duration_latest
       where(chunk_id: maximum(:chunk_id)).order(total_duration: :desc)
