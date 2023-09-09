0ebcf3337fc9af3116656fccea90ff4e1d0cdab9
Aono05 <k.aono0522@gmail.com>
2023-09-09 15:55:00 +0900

    [fix] Add Early Returns

--- app/validators/ngword_validator.rb
+++ app/validators/ngword_validator.rb
@@ -2,8 +2,14 @@ class NgwordValidator < ActiveModel::EachValidator
   NGWORD_REGEX = /(.)\1{4,}/.freeze
 
   def validate_each(record, attribute, value)
-    if value.present? && (value.match?(NGWORD_REGEX) || Obscenity.profane?(value))
-      record.errors.add(attribute, 'に5文字以上の繰り返し文字またはNGワードが含まれています。')
+    return value.blank?
+
+    if value.match?(NGWORD_REGEX)
+      record.errors.add(attribute, 'に5文字以上の繰り返し文字が含まれています。')
+    end
+
+    if Obscenity.profane?(value)
+      record.errors.add(attribute, 'にNGワードが含まれています。')
     end
   end
 end
