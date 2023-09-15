716306f0aa633b376b19faa155506cf7cd1fdd44
Aono05 <k.aono0522@gmail.com>
2023-09-16 07:50:59 +0900

    [Add] Add profile controller

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,28 @@
+class ProfilesController < ApplicationController
+  before_action :set_user,only: [:edit, :update]
+
+  def edit
+  end
+
+  def update
+    if @user.update!(user_params)
+      redirect_to profile_path, success: "プロフィールを更新しました"
+    else
+      flash.now[:danger] = "プロフィールを更新できませんでした"
+      render :edit
+    end
+  rescue StandardError => e
+    flash.now[:danger] = "エラーが発生しました: #{e.message}"
+    render :edit
+  end
+
+  private
+  def set_user
+    @user = User.find(current_user.id)
+  end
+
+  def user_params
+    params.require(:user).permit(:email,:name,:introduction)
+  end
+end
+
