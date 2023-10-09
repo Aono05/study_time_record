14c4706c68770a3eb3d6c43bda9c5afc9a1f057d
Aono05 <k.aono0522@gmail.com>
2023-10-09 13:36:01 +0900

    [fix]Fixed routing of cheer_messages from index to show

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'cheer_messages#index'
+  root to: 'cheer_messages#show'
   get 'study_times/index', to: 'study_times#index'
-  resources :study_times
+  resources :cheer_messages
+	resources :study_times
   resources :rankings
   resource :profile,only: [:show, :edit, :update]
 end
