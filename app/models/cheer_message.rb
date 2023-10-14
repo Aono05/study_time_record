749498ad1e634863b776892366ba81b88bc3ad0f
Aono05 <k.aono0522@gmail.com>
2023-10-14 21:41:56 +0900

    [fix] Uploader tied to associated model

--- app/models/cheer_message.rb
+++ app/models/cheer_message.rb
@@ -1,6 +1,7 @@
 class CheerMessage < ApplicationRecord
   belongs_to :user
   CHEER_MESSAGES = YAML.load_file("config/cheer_messages.yml")
+  mount_uploader :image, ImageUploader
 
   class << self
     def random_content
