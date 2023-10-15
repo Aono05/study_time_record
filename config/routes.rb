2c6fc8c502312eec33834d579410e3426205f862
Aono05 <k.aono0522@gmail.com>
2023-10-15 15:02:58 +0900

    [fix] Fix route from display to index for initial screen

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'cheer_messages#display'
-  get 'study_times/index', to: 'study_times#index'
+  root to: 'study_times#index'
   resources :cheer_messages
 	resources :study_times
   resources :rankings
