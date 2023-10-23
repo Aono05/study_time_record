b5696254cedefd412734c7db0ca71318a54e9365
Aono05 <k.aono0522@gmail.com>
2023-10-24 06:36:34 +0900

    [fix] Fixed method for displaying images

--- app/models/cheer_message.rb
+++ app/models/cheer_message.rb
@@ -1,27 +1,30 @@
 class CheerMessage < ApplicationRecord
   belongs_to :user
   CHEER_MESSAGES = YAML.load_file("config/cheer_messages.yml")
+  DEFAULT_IMAGE_NAME = "default.png".freeze
   mount_uploader :image, ImageUploader
 
+  def valid_image
+    if image.present?
+      image.url
+    else
+      "/assets/#{DEFAULT_IMAGE_NAME}"
+    end
+  end
+
   class << self
-    def user_random_content(user)
+    def random(user)
       if user.cheer_messages.present?
-        @random_cheer_message = user.cheer_messages.sample.content
+        user.cheer_messages.sample
       else
-        @random_cheer_message = random_content
+        random_content
       end
     end
 
+    private
     def random_content
-      CHEER_MESSAGES.sample
-    end
-
-    def cheer_message_image(user)
-      if user.cheer_messages.present?
-        @cheer_image = user.cheer_messages.sample.image.url
-      else
-        @cheer_image = ActionController::Base.helpers.asset_path("default.png")
-      end
+      content = CHEER_MESSAGES.sample
+      CheerMessage.new(content: content)
     end
   end
 end
