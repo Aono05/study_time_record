e92ce0b0853d1fe311f3c3860ad3d61b74572010
Aono05 <k.aono0522@gmail.com>
2024-02-14 06:50:02 +0900

    [fix] Changed to testing with FactoryBot

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :user do
-    
+    email                 {"hoge1@example.com"}
+    password              {"hoge1234567890@"}
+    password_confirmation {"hoge1234567890@"}
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "is valid with email and password" do
-    user = User.new(
-        email: "hoge1@example.com",
-        password: "hoge1234567890@"
-    )
+  let(:user) { build(:user) }
+  it "emailとパスワードが認証されること" do
     expect(user).to be_valid
   end
 end
