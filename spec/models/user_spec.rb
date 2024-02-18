df47115523d728601652d0b46039ffe336906d35
Aono05 <k.aono0522@gmail.com>
2024-02-18 17:22:14 +0900

    [add] Add test that password is valid

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) { build(:user) }
-  it "emailとパスワードが認証されること" do
-    expect(user).to be_valid
+  describe 'validation' do
+    context 'パスワードのバリデーション' do
+      let(:user) { build(:user) }
+      let(:password_error_message) { 'パスワードは、12文字以上64文字以下で英字小文字、数字、記号を含めてください。' }
+
+      it "emailとパスワードが有効であること" do
+        expect(user).to be_valid
+      end
+    end
   end
 end
