ca9a31b2c78ba1aaccf201ed7b91a7948942ee28
Aono05 <k.aono0522@gmail.com>
2024-02-19 06:58:07 +0900

    [fix] Added "introduction" to User information of FactoryBot.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,6 @@ FactoryBot.define do
     email                 {"hoge1@example.com"}
     password              {"hoge1234567890@"}
     password_confirmation {"hoge1234567890@"}
+    introduction          {"はじめましてhoge1です。"}
   end
 end
