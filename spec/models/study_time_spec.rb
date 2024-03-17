a6737607ff4db1eaca12aaf6158d33c8cdcd6919
Aono05 <k.aono0522@gmail.com>
2024-03-18 05:49:30 +0900

    [fx] Add test for "total_duration_for_user method"

--- spec/models/study_time_spec.rb
+++ spec/models/study_time_spec.rb
@@ -68,60 +68,104 @@ RSpec.describe StudyTime, type: :model do
   describe "#duration" do
     let(:study_time) { StudyTime.new(started_at: started_at, ended_at: ended_at) }
 
-      context "終了時間が設定されている場合" do
-        context "開始時間より終了時間が遅い場合" do
-          let(:started_at) { Time.zone.local(2023, 5, 25, 6, 0, 0) }
-          let(:ended_at) { Time.zone.local(2023, 5, 25, 6, 1, 0) }
-          let(:expected) { 1 }
+    context "終了時間が設定されている場合" do
+      context "開始時間より終了時間が遅い場合" do
+        let(:started_at) { Time.zone.local(2023, 5, 25, 6, 0, 0) }
+        let(:ended_at) { Time.zone.local(2023, 5, 25, 6, 1, 0) }
+        let(:expected) { 1 }
 
-          it "1分と表示される" do
-            expect(study_time.duration).to eq(expected)
-          end
-        end
-
-        context "開始時間と終了時間が同じ場合" do
-          let(:started_at) { Time.zone.local(2023, 5, 25, 6, 0, 0) }
-          let(:ended_at) { Time.zone.local(2023, 5, 25, 6, 0, 0) }
-          let(:expected) { 0 }
-
-          it "0分と表示される" do
-            expect(study_time.duration).to eq(expected)
-          end
-        end
-
-        context "開始時間が終了時間より後の場合" do
-          let(:started_at) { Time.zone.local(2023, 5, 25, 6, 1, 0) }
-          let(:ended_at) { Time.zone.local(2023, 5, 25, 6, 0, 0) }
-          let(:expected) { 0 }
-
-          it "0分と表示される" do
-            expect(study_time.duration).to eq(expected)
-          end
+        it "1分と表示される" do
+          expect(study_time.duration).to eq(expected)
         end
       end
 
-      context "終了時間が設定されていない場合" do
+      context "開始時間と終了時間が同じ場合" do
         let(:started_at) { Time.zone.local(2023, 5, 25, 6, 0, 0) }
-        let(:ended_at) { nil }
+        let(:ended_at) { Time.zone.local(2023, 5, 25, 6, 0, 0) }
         let(:expected) { 0 }
 
         it "0分と表示される" do
           expect(study_time.duration).to eq(expected)
         end
       end
+
+      context "開始時間が終了時間より後の場合" do
+        let(:started_at) { Time.zone.local(2023, 5, 25, 6, 1, 0) }
+        let(:ended_at) { Time.zone.local(2023, 5, 25, 6, 0, 0) }
+        let(:expected) { 0 }
+
+        it "0分と表示される" do
+          expect(study_time.duration).to eq(expected)
+        end
+      end
+    end
+
+    context "終了時間が設定されていない場合" do
+      let(:started_at) { Time.zone.local(2023, 5, 25, 6, 0, 0) }
+      let(:ended_at) { nil }
+      let(:expected) { 0 }
+
+      it "0分と表示される" do
+        expect(study_time.duration).to eq(expected)
+      end
+    end
   end
 
   describe "#before_started_on?" do
     let(:study_time) { StudyTime.new(started_at: started_at, ended_at: ended_at) }
+    let(:based_on) { Time.zone.local(2023, 5, 26, 6, 0, 0) }
 
-      context "started_atがbased_onの1日前と一致する場合" do
-        let(:based_on) { Time.zone.local(2023, 5, 26, 6, 0, 0) }
-        let(:started_at) { Time.zone.local(2023, 5, 25, 6, 0, 0) }
-        let(:ended_at) { Time.zone.local(2023, 5, 25, 7, 0, 0) }
+    context "started_atがbased_onの1日前の場合" do
+      let(:started_at) { Time.zone.local(2023, 5, 25, 6, 0, 0) }
+      let(:ended_at) { Time.zone.local(2023, 5, 25, 7, 0, 0) }
 
-        it "trueが返る" do
-          expect(study_time.before_started_on?(based_on)).to eq(true)
-        end
+      it "trueが返る" do
+        expect(study_time.before_started_on?(based_on)).to eq(true)
       end
+    end
+
+    context "started_atがbased_onと同じ日の場合" do
+      let(:started_at) { Time.zone.local(2023, 5, 26, 6, 0, 0) }
+      let(:ended_at) { Time.zone.local(2023, 5, 26, 7, 0, 0) }
+
+      it "falseが返る" do
+        expect(study_time.before_started_on?(based_on)).to eq(false)
+      end
+    end
+
+    context "started_atがbased_onの1日後の場合" do
+      let(:started_at) { Time.zone.local(2023, 5, 27, 6, 0, 0) }
+      let(:ended_at) { Time.zone.local(2023, 5, 27, 7, 0, 0) }
+
+      it "falseが返る" do
+        expect(study_time.before_started_on?(based_on)).to eq(false)
+      end
+    end
+  end
+
+  describe ".total_duration_for_user" do
+    let(:output) { described_class.total_duration_for_user(user) }
+    let(:user) { create(:user) }
+    let!(:study_times) {
+      [
+        create(
+          :study_time,
+          user: user,
+          started_at: Time.zone.local(2023, 5, 25, 6, 0, 0),
+          ended_at: Time.zone.local(2023, 5, 25, 7, 0, 0)
+        ),
+        create(
+          :study_time,
+          user: user,
+          started_at: Time.zone.local(2023, 5, 26, 6, 0, 0),
+          ended_at: Time.zone.local(2023, 5, 26, 8, 0, 0)
+        ),
+      ]
+    }
+    let(:expected) { 180.0 }
+
+    it "ユーザーの学習時間の合計が返る" do
+      expect(output).to eq(expected)
+    end
   end
 end
