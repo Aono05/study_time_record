26ca111612fca35277e1f000c59c28952da9ed6e
Aono05 <k.aono0522@gmail.com>
2024-03-27 21:27:00 +0900

    [fix] Fixed the contents of the ranking test

--- spec/models/ranking_spec.rb
+++ spec/models/ranking_spec.rb
@@ -2,70 +2,115 @@ require 'rails_helper'
 
 RSpec.describe Ranking, type: :model do
   describe ".create_total_week_duration_per_user" do
-    let(:user1) { create(:user, :a) }
-    let(:user2) { create(:user, :b) }
-    let(:study_time) { create(:study_time, user: user) }
-    let(:started_at) { Time.zone.local(2024, 1, 1, 0, 0, 0) }
-    let(:ended_at) { Time.zone.local(2024, 1, 7, 23, 59, 59) }
-
-    before do
-      create(
-        :study_time,
-        user: user1,
-        started_at: started_at,
-        ended_at: started_at + 1.hour
-      )
-      create(
-        :study_time,
-        user: user1,
-        started_at: started_at + 1.day,
-        ended_at: started_at + 1.day + 2.hours
-      )
-      create(
-        :study_time,
-        user: user2,
-        started_at: started_at,
-        ended_at: started_at + 2.hours
-      )
-    end
-
-    it "週間の学習時間ランキングが作成されること" do
-      expect {
-        described_class.create_total_week_duration_per_user(
-          started_at: started_at,
-          ended_at: ended_at
-        )
-      }.to change(described_class, :count).by(2)
-    end
-
-    it "週間の学習時間ランキングが正しく作成されること" do
+    let(:run) do
       described_class.create_total_week_duration_per_user(
         started_at: started_at,
         ended_at: ended_at
       )
-
-      expect(Ranking.find_by(user_id: user1.id).total_duration).to eq(180)
-      expect(Ranking.find_by(user_id: user2.id).total_duration).to eq(120)
     end
-  end
 
-  describe ".total_week_duration_latest" do
-    let(:user1) { create(:user, :a) }
-    let(:user2) { create(:user, :b) }
+    before do
+      run
+    end
 
-    it "最新の週間の学習時間ランキングが取得できること" do
-      create(:ranking, user: user1, total_duration: 180)
-      create(:ranking, user: user2, total_duration: 120)
+    context "集計期間が正しいこと" do
+      let(:result) do
+        described_class.total_week_duration_latest.find_by(user_id: user.id).total_duration
+      end
+      let(:based_at) { Time.current }
+      let(:started_at) { based_at.ago(1.week).beginning_of_hour }
+      let(:ended_at) { based_at.ago(1.hour).end_of_hour }
+      let!(:user) { create(:user) }
+      let!(:ranking) { create(:ranking, user: user) }
 
-      latest_rankings = described_class.total_week_duration_latest
+      context "勉強時間が基準日時から直近7日間のみ" do
+        let(:expected) { study_times.sum(&:duration) }
+        let!(:study_times) do
+          (0..6).map do |i|
+            create(
+              :study_time,
+              started_at: started_at + i.days,
+              ended_at: started_at + i.days + 1.hour,
+              user: user
+            )
+          end
+        end
 
-      # ランキングが期待通りの順序で取得されるか確認
-      expect(latest_rankings.pluck(:user_id)).to eq([user1.id, user2.id])
+        it "週間の学習時間ランキングが作成されること" do
+          expect(result).to eq(expected)
+        end
+      end
 
-      # ランキングが合計学習時間の降順で並び替えられているかどうかを検証
-      expect(latest_rankings.first.total_duration).to be >= latest_rankings.last.total_duration
+      context "勉強時間が基準日時から直近7日間および7日間より前を含む" do
+        let(:expected) { study_times.sum(&:duration) }
+        let!(:study_times) do
+          (0..6).map do |i|
+            create(
+              :study_time,
+              started_at: started_at + i.days,
+              ended_at: started_at + i.days + 1.hour,
+              user: user
+            )
+          end
+        end
+        let!(:out_of_range_study_time) do
+          create(
+            :study_time,
+            started_at: started_at - 1.days,
+            ended_at: started_at - 1.days + 1.hour,
+            user: user
+          )
+        end
+
+        it "週間の学習時間ランキングが作成されること" do
+          expect(result).to eq(expected)
+        end
+      end
+
+      context "勉強時間が基準日時から直近7日間および基準日時より後を含む" do
+        let(:expected) { study_times.sum(&:duration) }
+        let!(:study_times) do
+          (0..6).map do |i|
+            create(
+              :study_time,
+              started_at: started_at + i.days,
+              ended_at: started_at + i.days + 1.hour,
+              user: user
+            )
+          end
+        end
+        let!(:out_of_range_study_time) do
+          create(
+            :study_time,
+            started_at: started_at + 7.days,
+            ended_at: started_at + 7.days + 1.hour,
+            user: user
+          )
+        end
+
+        it "週間の学習時間ランキングが作成されること" do
+          expect(result).to eq(expected)
+        end
+      end
+
+      context "単一ユーザーで集計されること" do
+        let(:users_count) { 1 }
+        context "ユーザーが1人の場合" do
+          it "週間の学習時間ランキングが作成されること" do
+            expect(described_class.count).to eq(users_count)
+          end
+        end
+
+        context "ユーザーが2人以上の場合" do
+          let(:users_count) { 2 }
+          let!(:another_user) { create(:user) }
+          let!(:another_ranking) { create(:ranking, user: another_user) }
+
+          it "週間の学習時間ランキングが作成されること" do
+            expect(described_class.count).to eq(users_count)
+          end
+        end
+      end
     end
   end
 end
-
-
