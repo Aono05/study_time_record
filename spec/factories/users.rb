65dbe894af2f2c1834167396b3b3ef5d0f930796
Aono05 <k.aono0522@gmail.com>
2024-04-07 16:10:00 +0900

    [fix] Change of user information of FactoryBot

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :user do
     sequence(:id) { |n| n }
     sequence(:email) { |n| "user#{n}@example.com" }
-    password { "hoge1234567890@" }
-    password_confirmation { "hoge1234567890@" }
-    introduction { "はじめましてhoge1です。" }
+    password { "test1234567890@" }
+    password_confirmation { "test1234567890@" }
+    introduction { "はじめましてtest_userです。" }
   end
 end
