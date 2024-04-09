3d44669a3f854fb23392eaff30ccd584c98a193b
Aono05 <k.aono0522@gmail.com>
2024-04-09 21:38:50 +0900

    [fix] Fix error messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable
 
   validates :email, presence: true
-  validates :password, presence: true, on: :update, allow_blank: true 
+  validates :password, presence: true, on: :update, allow_blank: true
   # FIXME: allow_blankしていると空文字を許容してしまうので許容しないように修正する必要がある
   validate :password_complexity
   validates :introduction, length: { maximum: 200 }
@@ -16,6 +16,6 @@ class User < ApplicationRecord
     return if password.blank?
     return if password =~ PASSWORD_REGEXP
 
-    errors.add :password, 'パスワードは、12文字以上64文字以下で英字小文字、数字、記号を含めてください。'
+    errors.add :password, I18n.t('activerecord.errors.models.user.attributes.password.blank')
   end
 end
