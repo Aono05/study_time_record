165e9f6b2f107c5c53f081220161983740575389
Aono05 <k.aono0522@gmail.com>
2024-04-29 14:31:40 +0900

    [fix] Check for duplicate emails

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def create
     if params[:user][:password] == params[:user][:password_confirmation]
-      super
+      if User.exists?(email: params[:user][:email])
+        flash[:alert] = "既に登録されているメールアドレスです"
+        redirect_to new_user_registration_path
+      else
+        super
+      end
     else
       flash[:alert] = "パスワードと確認用パスワードが一致しません"
       redirect_to new_user_registration_path
